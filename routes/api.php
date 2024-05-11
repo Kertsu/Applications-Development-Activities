c785914f7a360410b1993ab2dd89e7fb7579c455
K3rtsu <kurtddaniel@gmail.com>
2024-05-11 14:40:24 +0800

    add task 1

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/task-1', function () {
+    # SELECT * FROM students;
+    $students = DB::table('students')->get();
+    return response()->json($students);
+});
+
+
